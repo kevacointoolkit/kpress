b9dfd9b6f5d4be42984b80842cd9d5e0db2ad3a0
kawww <39939137+kawww@users.noreply.github.com>
2022-08-03 07:32:40 +0800

    Add files via upload

---
+++ kpress.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+##sudo -s
+
+apt install apache2 -y
+
+systemctl status apache2
+
+apt install php libapache2-mod-php -y
+
+systemctl restart apache2
+
+rm main.zip
+
+wget https://github.com/kevacointoolkit/kpress/archive/refs/heads/main.zip --no-check-certificate
+
+rm -rf kpress-main/*
+
+apt install unzip
+
+rm -rf /var/www/html/*
+
+unzip main.zip
+
+cp kpress-main/* /var/www/html/ -r
+
+sudo apt-get install php-bcmath -y
+
+sudo apt-get install php-curl -y
+
+service apache2 reload
+
+cd /var/www/
+
+chmod 777 * -R
+
